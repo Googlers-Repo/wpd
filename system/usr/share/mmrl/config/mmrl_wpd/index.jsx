41dda3eee447db121c0b52998ae570de2ce3cbbb
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-02-29 11:34:03 +0100

    Update index.jsx

--- system/usr/share/mmrl/config/mmrl_wpd/index.jsx
+++ system/usr/share/mmrl/config/mmrl_wpd/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Page, Toolbar, Ansi } from "@mmrl/ui";
-import { useActivity } from "@mmrl/hooks";
-import { Save } from "@mui/icons-material";
-import { Stack, Box } from "@mui/material";
-import BuildConfig from "@mmrl/buildconfig";
+import { Page, Toolbar } from "@mmrl/ui";
+import { useActivity, useNativeStorage } from "@mmrl/hooks";
+import {
+    List,
+    ListItem,
+    ListItemText,
+    Divider
+} from "@mui/material";
 import Terminal from "@mmrl/terminal";
-import { write } from "@mmrl/sufile"
 
 const __wpd = `[ "\$(whoami)" != "root" ] && { echo "root required"; exit 1; }
 configs=( /data/misc/wifi/WifiConfigStore.xml /data/misc/apexdata/com.android.wifi/WifiConfigStore.xml )
@@ -16,19 +18,32 @@ for z in \${configs[@]}; do
   fi
 done
 SSID=(\$(grep 'name="SSID"' \$config | sed "s/.*>&quot;//;s/&quot;.*//;s/ /-_-/g"))
-PSK=(\$(grep 'name="PreSharedKey"' \$config | sed "s/<null.*/\x1b[31mNONE\x1b[40m/;s/.*>&quot;/\x1b[32m/;s/&quot;.*/\x1b[40m/;s/ /-_-/g"))
-
-echo "WiFi Password Viewer"
-echo "veez21 @ xda-developers"
-echo "MMRL \x1b[91m(\x1b[0m\x1b[93m\$MMRL_VER_NAME\x1b[91m)\x1b[0m version by Der_Googler"
-echo " "
+PSK=(\$(grep 'name="PreSharedKey"' \$config | sed "s/<null.*/NONE/;s/.*>&quot;//;s/&quot;.*//;s/ /-_-/g"))
 for i in \${!SSID[@]}; do
-   echo "\${SSID[\$i]} - \${PSK[\$i]}" | sed "s/-_-/ /g"
+   echo "\${SSID[\$i]},\"\${PSK[\$i]}\"" | sed "s/-_-/ /g"
 done`
 
+
+function RenderToolbar() {
+    const { context } = useActivity()
+    return (
+        <Toolbar modifier="noshadow" sx={{
+            background: "rgb(188,2,194)",
+            background: "linear-gradient(22deg, rgba(188,2,194,1) 0%, rgba(74,20,140,1) 100%)"
+        }}>
+            <Toolbar.Left>
+                <Toolbar.BackButton onClick={context.popPage} />
+            </Toolbar.Left>
+            <Toolbar.Center>
+                WiFi Password Viewer
+            </Toolbar.Center>
+        </Toolbar>
+    )
+}
+
 function Wpd() {
-    const { context } = useActivity();
     const [lines, setLines] = React.useState([]);
+    const [hidePasswords, setHidePasswords] = useNativeStorage("wpd_hide_passwords", true)
 
     const addLine = (line) => {
         setLines((lines) => [...lines, line]);
@@ -36,25 +51,6 @@ function Wpd() {
 
     const saveLog = () => {
         write("/data/adb/wpd.log", lines.join("\n"))
-    }
-
-    const renderToolbar = () => {
-        return (
-            <Toolbar modifier="noshadow" sx={{
-                background: "rgb(188,2,194)",
-                background: "linear-gradient(22deg, rgba(188,2,194,1) 0%, rgba(74,20,140,1) 100%)"
-            }}>
-                <Toolbar.Left>
-                    <Toolbar.BackButton onClick={context.popPage} />
-                </Toolbar.Left>
-                <Toolbar.Center>
-                    WiFi Password Viewer
-                </Toolbar.Center>
-                <Toolbar.Right>
-                    <Toolbar.Button onClick={saveLog} icon={Save} />
-                </Toolbar.Right>
-            </Toolbar>
-        )
     }
 
     const startLog = React.useMemo(() => {
@@ -67,7 +63,11 @@ function Wpd() {
             command: __wpd,
             env: envp,
             onLine: (line) => {
-                addLine(line);
+                line = line.split(",")
+                addLine({
+                    ssid: line[0],
+                    psk: line[1]
+                });
             },
             onExit: (code) => { },
         });
@@ -76,33 +76,27 @@ function Wpd() {
     return (
         <Page
             onShow={startLog}
-            renderToolbar={renderToolbar}
+            renderToolbar={RenderToolbar}
             modifier="noshadow">
-            <div
-                style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                }}>
-                <Stack
-                    style={{
-                        whiteSpace: "pre",
-                        flex: "0 0 100%",
-                        color: "white",
-                        height: "100%",
-                    }}
-                    direction="column"
-                    justifyContent="flex-start"
-                    alignItems="stretch"
-                    spacing={0}
-                >
-                    {lines.map((line) => (
-                        <Box component={Ansi} sx={{
-                            ml: 1,
-                            mr: 1,
-                        }}>{line}</Box>
-                    ))}
-                </Stack>
-            </div>
+            <List subheader={<ListSubheader>Settings</ListSubheader>}>
+                <ListItem>
+                    <ListItemText primary="Hide passwords" />
+                    <Switch checked={hidePasswords} onChange={(e) => setHidePasswords(e.target.checked)} />
+                </ListItem>
+            </List>
+            <Divider />
+            <List subheader={<ListSubheader>Passwords</ListSubheader>}>
+                {lines.map((line) => (
+                    <ListItem>
+                        <ListItemText sx={{
+                            "& .MuiListItemText-secondary": {
+                                WebkitTextSecurity: hidePasswords ? "disc" : "none",
+                                wordWrap: "break-word"
+                            }
+                        }} primary={line.ssid} secondary={line.psk} />
+                    </ListItem>
+                ))}
+            </List>
         </Page>
     );
 }
